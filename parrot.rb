3993fc96a1e6608e9731f98d704dc6d725ffa151
krishnendu basak <krisauz17@yahoo.com>
2022-05-07 14:24:06 +1000

    parrot-ruby

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot (word = "Squawk!")
+    puts word
+    word
+end
+parrot
+
+def parrot (word = "Squawk!")
+    puts word
+    return word
+end
+parrot
